105e08b194d78373d001519ae07ac7dc84e78d16
bhavanishankar6847-web <bhavanishankar6847@gmail.com>
2026-09-11 10:20:00 +0530

    Update backend app

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5177",
         "http://localhost:5178",
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
     ],
     allow_credentials=True,
     allow_methods=["*"],
